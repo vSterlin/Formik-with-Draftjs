107e5aba2f637c672c840b18f067427fd1f34c38
Vladimir Sterlin <vsterlin21@gmail.com>
2022-01-17 01:56:26 -0800

    extract state change handler into func

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,31 +1,35 @@
 import { Editor as _Editor } from "react-draft-wysiwyg";
 import { EditorState, convertToRaw } from "draft-js";
 import { stateFromHTML } from "draft-js-import-html";
-import draftToHtml from 'draftjs-to-html';
+import draftToHtml from "draftjs-to-html";
 import { FC, useState } from "react";
-import { FormikErrors } from "formik"
-import { FakeData } from "../App"
+import { FormikErrors } from "formik";
+import { FakeData } from "../App";
 
-
-
-const Editor: FC<{value: string, setFieldValue: (field: string, value: any, shouldValidate?: boolean | undefined) => Promise<void> | Promise<FormikErrors<FakeData>>
-}> = ({value, setFieldValue}) => {
-
-
+const Editor: FC<{
+  value: string;
+  setFieldValue: (
+    field: string,
+    value: any,
+    shouldValidate?: boolean | undefined
+  ) => Promise<void> | Promise<FormikErrors<FakeData>>;
+}> = ({ value, setFieldValue }) => {
   const contentState = stateFromHTML(value);
   const [editorState, setEditorState] = useState(
     EditorState.createWithContent(contentState)
   );
 
+  const onEditorStateChange = (editorState: EditorState) => {
+    setEditorState(editorState);
+    const rawContent = convertToRaw(editorState.getCurrentContent());
+    setFieldValue("description", draftToHtml(rawContent));
+  };
+
   return (
     <div>
-       <_Editor
+      <_Editor
         editorState={editorState}
-        onEditorStateChange={(editorState) => {
-          setEditorState(editorState) 
-          const rawContent = convertToRaw(editorState.getCurrentContent())
-          setFieldValue("description", draftToHtml(rawContent))
-        }}
+        onEditorStateChange={onEditorStateChange}
         toolbar={{
           inline: { inDropdown: true },
           list: { inDropdown: true },
@@ -35,7 +39,7 @@ const Editor: FC<{value: string, setFieldValue: (field: string, value: any, shou
         }}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Editor
+export default Editor;
